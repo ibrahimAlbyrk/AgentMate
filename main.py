db7f5cf9ec58b7dbbe9a5b995b04e6622b798fdf
ibrahimAlbayrak <ibrahimalbayrak812@gmail.com>
2025-03-27 17:23:02 +0300

    proxy header fix

--- main.py
+++ main.py
@@ -54,4 +54,4 @@ app.include_router(auth_router.router)
 app.include_router(webhook.router)
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="127.0.0.1", port=5000)
+    uvicorn.run("main:app", host="127.0.0.1", port=5000, proxy_headers=True)
