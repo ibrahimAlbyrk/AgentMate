ccf4960476ceaf46895b75c261005661778210eb
ibrahimAlbayrak <ibrahimalbayrak812@gmail.com>
2025-03-28 21:44:10 +0300

    added abc

--- Base/agent_base.py
+++ Base/agent_base.py
@@ -1,9 +1,12 @@
+from abc import ABC, abstractmethod
+
 from Core.logger import LoggerCreator
 from Interfaces.agent_interface import IAgent
 
-class BaseAgent(IAgent):
+class BaseAgent(IAgent, ABC):
     def __init__(self, name: str):
         self.logger = LoggerCreator.create_advanced_console(name)
 
+    @abstractmethod
     async def run(self, uid: str):
         raise NotImplementedError("run() method must be implemented by the subclass")
