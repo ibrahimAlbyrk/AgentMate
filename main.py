437f84bcf87ec2802ff2686b021253b8d9aea0f9
ibrahimAlbayrak <ibrahimalbayrak812@gmail.com>
2025-03-28 17:20:30 +0300

    set reload true

--- main.py
+++ main.py
@@ -51,4 +51,4 @@ app.include_router(webhook.router)
 app.include_router(websocket_router.router)
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="127.0.0.1", port=6000, proxy_headers=True)
+    uvicorn.run("main:app", host="127.0.0.1", port=6000, reload=True)
