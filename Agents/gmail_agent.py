c8b4093f099966835c7b519dafc4df4fab5c6cc8
ibrahimAlbayrak <ibrahimalbayrak812@gmail.com>
2025-04-01 15:19:37 +0300

    test event

--- Agents/gmail_agent.py
+++ Agents/gmail_agent.py
@@ -28,7 +28,12 @@ class GmailAgent(IAgent):
         pass
 
     def _handle_new_email_messages(self, event):
-        print(event)
+        payload = json.loads(event)
+        sender = payload["sender"]
+        subject = payload["subject"]
+        content = payload["messageText"]
+
+        print(f"sender: {sender},\n subject: {subject}\n, content: {content}")
 
 # class GmailAgent(IAgent):
 #     def __init__(self, uid: str):
