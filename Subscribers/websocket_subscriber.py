2eb9460737173169a605e1dc708b37d0915aab51
ibrahimAlbayrak <ibrahimalbayrak812@gmail.com>
2025-04-13 21:30:20 +0300

    websocket subscriber has been fixed

--- Subscribers/websocket_subscriber.py
+++ Subscribers/websocket_subscriber.py
@@ -9,30 +9,32 @@ from Subscribers.base_subscriber import BaseSubscriber
 
 logger = LoggerCreator.create_advanced_console("WebSocketSubscriber")
 
-event_bus = EventBus()
-
 
 class WebSocketSubscriber(BaseSubscriber):
-    async def register(self):
+    def __init__(self):
+        self.event_bus = None
+
+    async def setup(self, **services):
+        self.event_bus = services['event_bus']
+
         self.event_bus.subscribe("websocket.gmail.memory", _handle_memory_send)
 
+    async def _handle_memory_send(self, raw_data: str):
+        data = self._get_data(raw_data)
+        if not data:
+            return
 
-async def _handle_memory_send(raw_data: str):
-    data = _get_data(raw_data)
-    if not data:
-        return
+        uid = data.get("uid", "")
+        memories = data.get("memories", [])
 
-    uid = data.get("uid", "")
-    memories = data.get("memories", [])
+        await send_message_to_active_connection(uid, message_type="gmail.memory", message={"memories": memories})
 
-    await send_message_to_active_connection(uid, message_type="gmail.memory", message={"memories": memories})
-
-
-def _get_data(raw_data: str) -> dict:
-    try:
-        payload = json.loads(raw_data)
-        uid = payload["uid"]
-        memories = payload["memories"]
-        return {"uid": uid, "memories": memories}
-    except Exception as e:
-        logger.error(f"Error handling memory data: {str(e)}")
+    @staticmethod
+    def _get_data(raw_data: str) -> dict:
+        try:
+            payload = json.loads(raw_data)
+            uid = payload["uid"]
+            memories = payload["memories"]
+            return {"uid": uid, "memories": memories}
+        except Exception as e:
+            logger.error(f"Error handling memory data: {str(e)}")
