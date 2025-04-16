528bdb7aa766f52ab94665f7f2e9b1fafd276619
ibrahimAlbayrak <ibrahimalbayrak812@gmail.com>
2025-04-17 00:25:43 +0300

    Update auth_router.py

--- DB/Routers/auth_router.py
+++ DB/Routers/auth_router.py
@@ -116,11 +116,12 @@ async def service_login(uid: str, service: str, session: AsyncSession = Depends(
         redirect_uri = settings.POST_LOGIN_REDIRECT.format(uid=uid, service=service)
         return RedirectResponse(url=redirect_uri)
 
-    google_integration_id = 'ac_qmAy5MZ7Mhep'
+    google_integration_id = '5cacfd7b-44b0-45fd-a998-d30f38e665b7'
+    integration = toolset.get_integration(google_integration_id)
 
     service_name = settings.SERVICES.get(service)
     entity = toolset.get_entity(uid)
-    conn_req = toolset.initiate_connection(app=service_name, integration_id=google_integration_id, entity_id=uid, redirect_url=f"{settings.BASE_URI}/api/{service}/callback?uid={uid}")
+    conn_req = toolset.initiate_connection(integration_id=integration.id, entity_id=uid, redirect_url=f"{settings.BASE_URI}/api/{service}/callback?uid={uid}")
     redirect_uri = conn_req.redirectUrl
 
     logger.debug(f"[{service}] Redirecting UID {uid} to OAuth flow")
