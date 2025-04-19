b12c1764a412d48470c7ffb24995af4369a18ec2
ibrahimAlbayrak <ibrahimalbayrak812@gmail.com>
2025-04-19 20:54:47 +0300

    Update config.py

--- Core/config.py
+++ Core/config.py
@@ -122,10 +122,12 @@ class AppSettings(BaseSettings):
     services: ServiceSettings = Field(default_factory=ServiceSettings)
     api: ApiSettings = Field(default_factory=ApiSettings)
 
+    # Service mappings
     _service_apps: Dict[str, App] = PrivateAttr(
         default_factory=lambda: {"gmail": App.GMAIL}
     )
 
+    # Configuration models for services
     config_models: Dict[str, Type[BaseModel]] = {
         "gmail": GmailConfig,
         # "notion": NotionConfig
