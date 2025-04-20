ea56def74320db85a3a32425582ef398bcf05f6f
ibrahimAlbayrak <ibrahimalbayrak812@gmail.com>
2025-04-20 23:23:08 +0300

    Update composio_router.py

--- Routers/composio_router.py
+++ Routers/composio_router.py
@@ -5,6 +5,7 @@ from DB.database import get_db
 from DB.Models.user_settings import UserSettings
 from DB.Services.user_settings_service import UserSettingsService
 
+from Core.logger import LoggerCreator
 from Core.agent_manager import agent_manager
 from Core.agent_factory import AgentFactory
 
@@ -16,6 +17,7 @@ from fastapi import APIRouter, HTTPException, Depends, Request
 
 router = APIRouter(prefix="/composio", tags=["Composio"])
 
+logger = LoggerCreator.create_advanced_console("ComposioRouter")
 
 @router.post("/webhook")
 async def webhook(request: Request, session: AsyncSession = Depends(get_db)):
@@ -31,6 +33,7 @@ async def webhook(request: Request, session: AsyncSession = Depends(get_db)):
 
     user: UserSettings = await UserSettingsService.get_by_service_id(session, connection_id)
     if not user:
+        logger.debug("User not found")
         return {"status": "error", "message": "User not found"}
 
     uid = user.uid
@@ -38,19 +41,24 @@ async def webhook(request: Request, session: AsyncSession = Depends(get_db)):
 
     agent_type = AgentFactory.registry.get(agent_name, None)
     if not agent_type:
+        logger.debug("Agent Type not found")
         return {"status": "error", "message": "Agent not found"}
 
     agent: IAgent = agent_manager.get_agent(uid, agent_name, agent_type)
     if not agent:
+        logger.debug("Agent not found")
         return {"status": "error", "message": "Agent not found"}
 
     listener: callable = agent.listeners.get(listener_type)
     if not listener:
+        logger.debug("Listener not found")
         return {"status": "error", "message": "Listener not found"}
 
     if asyncio.iscoroutinefunction(listener):
         await listener(data)
     else:
         listener(data)
+
+    logger.debug("called listener")
 
     return {"status": "success", "message": f"{listener_type} is triggered"}
