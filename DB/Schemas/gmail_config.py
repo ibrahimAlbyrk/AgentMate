8ef988ae9f05040cc7e47ac4e4c91f931f465ba9
ibrahimAlbayrak <ibrahimalbayrak812@gmail.com>
2025-04-20 21:07:22 +0300

    removed old datas

--- DB/Schemas/gmail_config.py
+++ DB/Schemas/gmail_config.py
@@ -3,7 +3,5 @@ from pydantic import BaseModel
 
 
 class GmailConfig(BaseModel):
-    mail_check_interval: int = 60
-    mail_count: int = 3
     important_categories: List[str] = []
     ignored_categories: List[str] = []
