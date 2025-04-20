0a05ec89815f6ff42d25d0bb7943b556cd2d59e8
ibrahimAlbayrak <ibrahimalbayrak812@gmail.com>
2025-04-21 02:14:07 +0300

    Added admin router

---
+++ Routers/admin_router.py
@@ -0,0 +1,18 @@
+from fastapi import APIRouter, Depends
+
+from sqlalchemy import select, func, distinct
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from DB.database import get_db
+from DB.Models.user_settings import UserSettings
+
+router = APIRouter(prefix="admin", tags=["Admin"])
+
+
+@router.get("/user-count")
+async def get_user_count(session: AsyncSession = Depends(get_db)):
+    result = await session.execute(
+        select(func.count(distinct(UserSettings.uid)))
+    )
+    count = result.scalar()
+    return {"user_count": count}
