3556922e65a113e2059473aa7a45728b4c3e2205
ibrahimAlbayrak <ibrahimalbayrak812@gmail.com>
2025-04-20 21:09:39 +0300

    Update webhook_router.py

--- Routers/webhook_router.py
+++ Routers/webhook_router.py
@@ -62,7 +62,8 @@ async def update_settings(uid: str, service: str, request: Request, db: AsyncSes
     config_model = settings.get_config_model(service)
     if config_model:
         try:
-            config = config_model(**config_data).model_dump()
+            config_instance = config_model_cls(**config_data)
+            config = config_instance.model_dump()
         except ValidationError as e:
             raise HTTPException(status_code=400, detail=f"Invalid {service} config: {str(e)}")
     else:
