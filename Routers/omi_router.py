6f64fcbc308ce476794585d28bb3b1013392e14c
ibrahimAlbayrak <ibrahimalbayrak812@gmail.com>
2025-04-21 05:50:40 +0300

    added transcript processed omi listener

--- Routers/omi_router.py
+++ Routers/omi_router.py
@@ -1,10 +1,15 @@
-from DB.database import get_db
+from Core.logger import LoggerCreator
+
 from sqlalchemy.ext.asyncio import AsyncSession
+
+from DB.database import get_db
 from DB.Services.user_settings_service import UserSettingsService
+
 from fastapi import APIRouter, HTTPException, Depends
 
 router = APIRouter(tags=["Omi"])
 
+logger = LoggerCreator.create_advanced_console("OmiRouter")
 
 @router.get("/setup-complete")
 async def is_setup_completed(uid: str, session: AsyncSession = Depends(get_db)):
@@ -13,3 +18,7 @@ async def is_setup_completed(uid: str, session: AsyncSession = Depends(get_db)):
 
     has_user = await UserSettingsService.has_any(session, uid)
     return {"is_setup_completed": has_user}
+
+@router.post("/transcript-processed")
+async def transcript_processed(uid: str, transcript: dict, session: AsyncSession = Depends(get_db)):
+    logger.debug(transcript)
