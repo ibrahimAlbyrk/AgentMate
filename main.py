7c0765c2ef46dea4e4176b9ac0655abd4d56d905
ibrahimAlbayrak <ibrahimalbayrak812@gmail.com>
2025-04-21 07:00:11 +0300

    Update main.py

--- main.py
+++ main.py
@@ -73,4 +73,4 @@ _include_routers([
 ])
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="127.0.0.1", port=6000)
+    uvicorn.run("main:app", host="127.0.0.1", port=6000, log_level="warning")
