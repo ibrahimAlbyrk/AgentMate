532f05329d58e131474ee564f6a6e45dca1e0b18
ibrahimAlbayrak <ibrahimalbayrak812@gmail.com>
2025-04-21 06:12:31 +0300

    Update omi_router.py

--- Routers/omi_router.py
+++ Routers/omi_router.py
@@ -21,7 +21,7 @@ async def is_setup_completed(uid: str, session: AsyncSession = Depends(get_db)):
 
 @router.post("/transcript-processed")
 async def transcript_processed(uid: str, transcript: dict, session: AsyncSession = Depends(get_db)):
-    wake_words  = ["hey omi", "omi", "yo omi", "ok omi", "hey, omi", "hey, omi.", "omi.", "hey omi."]
+    wake_words  = ["hey omi", "omi", "yo omi", "ok omi", "hey, omi", "hey, omi.", "omi.", "hey omi.", "ome", "hey ome"]
 
     segments = transcript["segments"]
     for segment in segments:
