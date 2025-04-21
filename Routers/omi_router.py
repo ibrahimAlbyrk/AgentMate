fa5c6806a3c3586e7146102fead0d62206ed4000
ibrahimAlbayrak <ibrahimalbayrak812@gmail.com>
2025-04-21 06:02:17 +0300

    Update omi_router.py

--- Routers/omi_router.py
+++ Routers/omi_router.py
@@ -25,7 +25,7 @@ async def transcript_processed(uid: str, transcript: dict, session: AsyncSession
     for segment in segments:
         text = segment["text"].lower()
         logger.debug(text)
-        if "hey, agent mate" in text:
+        if "hey, agent mate." in text:
             return {"message": "hey {user} or {{user}}"}
 
     return {}
