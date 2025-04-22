474837a9408f8d28b0a501b60196f355f7174e6e
ibrahimAlbayrak <ibrahimalbayrak812@gmail.com>
2025-04-22 07:41:33 +0300

    added get users info api

--- Routers/admin_router.py
+++ Routers/admin_router.py
@@ -1,8 +1,11 @@
+from typing import Any, Dict
+
 from fastapi import APIRouter, Depends
 
 from sqlalchemy import select, func, distinct
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from DB.Services.user_settings_service import UserSettingsService
 from DB.database import get_db
 from DB.Models.user_settings import UserSettings
 
@@ -16,3 +19,15 @@ async def get_user_count(session: AsyncSession = Depends(get_db)):
     )
     count = result.scalar()
     return {"user_count": count}
+
+@router.get("/users-info")
+async def get_users_info(session: AsyncSession = Depends(get_db)):
+    users_data = await UserSettingsService.get_all_users(session)
+
+    users: Dict[str, Dict[str, Any]] = {}
+
+    for user_data in users_data:
+        if user_data.uid in users.keys():
+            users[user_data.uid] = {"service": user_data.service_name, "is logged in": user_data.is_logged_in}
+
+    return {"users": users}
