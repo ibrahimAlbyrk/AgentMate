314328f1864949ec40bacf37b4dfe094ac047381
ibrahimAlbayrak <ibrahimalbayrak812@gmail.com>
2025-04-24 08:55:38 +0300

    Update agent_factory.py

--- Core/agent_factory.py
+++ Core/agent_factory.py
@@ -1,5 +1,5 @@
 from Agents.Gmail.gmail_agent import GmailAgent
-# from Agents.Notion.notion_agent import NotionAgent
+from Agents.Notion.notion_agent import NotionAgent
 # from Agents.Calendar.calendar_agent import CalenderAgent
 # from Agents.Slack.slack_agent import SlackAgent
 
