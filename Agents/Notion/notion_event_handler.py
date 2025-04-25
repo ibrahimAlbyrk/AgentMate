e23770d90c2e25251220d05efa751ff274b34889
ibrahimAlbayrak <ibrahimalbayrak812@gmail.com>
2025-04-25 14:38:18 +0300

    Update notion_event_handler.py

--- Agents/Notion/notion_event_handler.py
+++ Agents/Notion/notion_event_handler.py
@@ -2,11 +2,15 @@ import json
 import asyncio
 from typing import Dict, Any, Optional, Callable
 
+from DB.database import get_db
+
 from Agents.agent_event_handler import AgentEventHandler
 
 from Core.EventBus import EventBus
 from Core.logger import LoggerCreator
 from Core.Models import EventType, Event
+from DB.Services.user_settings_service import UserSettingsService
+
 
 class NotionEventHandler(AgentEventHandler):
     def __init__(self, uid: str, event_bus: Optional[EventBus] = None):
@@ -29,6 +33,10 @@ class NotionEventHandler(AgentEventHandler):
 
 
     def get_events(self) -> Dict[str, Dict[str, Any]]:
+
+        user_config: Dict[str, Any] = UserSettingsService.get_config(get_db, self.uid, "notion")
+        print(user_config)
+
         return {
             "NOTION_PAGE_ADDED_TRIGGER": {
                 "handler": self.handle_new_page_added,
