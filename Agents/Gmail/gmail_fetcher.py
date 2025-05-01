9fc0ca1302a041eaf49ca2f5ceabe58b951df6f6
ibrahimAlbayrak <ibrahimalbayrak812@gmail.com>
2025-05-01 19:24:10 +0300

    refactor(Agents): refactored by base agent fetcher

--- Agents/Gmail/gmail_fetcher.py
+++ Agents/Gmail/gmail_fetcher.py
@@ -1,40 +1,41 @@
 from typing import Dict, Any, List, Optional
 
 from Core.logger import LoggerCreator
+
 from Agents.LLM.llm_agent import LLMAgent
+from Agents.agent_fetcher import AgentFetcher
 
 
-class GmailFetcher:
+class GmailFetcher(AgentFetcher):
     """
     This class encapsulates all the logic for retrieving emails from Gmail,
     including fetching email lists and individual emails.
     """
 
     def __init__(self, llm_agent: LLMAgent, include_labels: List[str] = None):
-        self.llm = llm_agent
+        super().__init__(llm_agent)
         self.include_labels = include_labels or ['INBOX']
-        self.logger = LoggerCreator.create_advanced_console("GmailFetcher")
 
     async def get_emails(self, limit: int) -> Dict[str, Any]:
-        try:
-            output = await self.llm.run_action("get_emails", max_results=limit, label_ids=self.include_labels)
-            return output['data']
-        except Exception as e:
-            self.logger.error(f"Error fetching emails: {str(e)}")
+        messages = await self.fetch("get_emails", limit=limit)
+
+        if messages:
+            return messages
+        else:
             return {'messages': []}
 
     async def get_emails_subjects(self, limit: int) -> Dict[str, Any]:
-        try:
-            output = await self.llm.run_action("get_emails_subjects", max_results=limit, label_ids=self.include_labels)
-            return output['data']
-        except Exception as e:
-            self.logger.error(f"Error fetching email subjects: {str(e)}")
+        messages = await self.fetch("get_emails_subjects", max_results=limit, label_ids=self.include_labels)
+
+        if messages:
+            return messages
+        else:
             return {'messages': []}
 
     async def get_email_by_message_id(self, message_id: str) -> Dict[str, Any]:
-        try:
-            output = await self.llm.run_action("get_email_by_message_id", message_id=message_id)
-            return output['data']
-        except Exception as e:
-            self.logger.error(f"Error fetching email by message ID: {str(e)}")
+        messages = await self.fetch("get_email_by_message_id", message_id=message_id)
+
+        if messages:
+            return messages
+        else:
             return {}
